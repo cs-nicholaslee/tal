c4cfb13d574e67f735639853810292fb1c501c81
cs-nicholaslee <nicholas.lee@hashicorp.com>
2023-04-18 11:52:43 +0800

    refact

--- modules/hashicat/main.tf
+++ modules/hashicat/main.tf
@@ -29,17 +29,17 @@ resource "azurerm_resource_group" "dbresourcegroup" {
   }
 }
 
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "${var.prefix}-vnet"
-#   location            = azurerm_resource_group.myresourcegroup.location
-#   address_space       = [var.address_space]
-#   resource_group_name = azurerm_resource_group.myresourcegroup.name
-# }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  location            = azurerm_resource_group.myresourcegroup.location
+  address_space       = [var.address_space]
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+}
 
 resource "azurerm_subnet" "subnet" {
   name = "${var.prefix}-subnet"
-  # virtual_network_name = azurerm_virtual_network.vnet.name
-  virtual_network_name = local.vnet_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  //virtual_network_name = local.vnet_name
   resource_group_name  = azurerm_resource_group.myresourcegroup.name
   address_prefixes     = [var.subnet_prefix]
 }
